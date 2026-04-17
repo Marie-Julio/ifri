ce640b7b613cd89568839d0b0f1942703d01405e
Marie-Julio <mariejuliobricedjego@gmail.com>
2026-04-17 12:38:47 +0100

    Depot 6

--- app/Service/AllConstants.php
+++ app/Service/AllConstants.php
@@ -4,12 +4,12 @@ namespace App\Service;
 class AllConstants {
 
     public static $APP_NAME = "IFRI";
-    public static $APP_URL = 'https://ifri-uac.bj/';
-    public static $API_URL = "https://api.ifri-uac.bj/";
+    public static $APP_URL = 'https://uac-ifri.bj/';
+    public static $API_URL = "https://api.uac-ifri.bj/";
     
     public static $DB_NAME = "ifridbnew";
-    public static $DB_USERNAME = "ifridbuser";
-    public static $DB_PASSWORD = "svGdfb2Gp7URC071"; 
+    public static $DB_USERNAME = "root";
+    public static $DB_PASSWORD = "password"; 
     public static $DB_HOST = "localhost";
 
 }
